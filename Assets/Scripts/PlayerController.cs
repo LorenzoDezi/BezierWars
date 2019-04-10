ccf5dc7c66758baead88dd7d04d3c50a18f761af
Lorenzo Dezi <lorenzo.dezi@studenti.unicam.it>
2019-04-11 01:00:53 +0200

    First commit - PlayerController and CameraController added. Add placeholder graphics.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private string straightMovementAxis;
+    [SerializeField]
+    private string orientationAxis;
+    [SerializeField]
+    private string shootAxis;
+    private bool isAccelerating = true;
+
+    [SerializeField]
+    private float maxVelocity;
+    [SerializeField]
+    private float brakeIntensity = 5f;
+    [SerializeField]
+    private float enginePower = 5f;
+    [SerializeField]
+    private float torqueIntensity = 5f;
+    [SerializeField]
+    private float maxAngularVelocity;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        float straightMovInput = Input.GetAxis(straightMovementAxis);
+        float orientInput = Input.GetAxis(orientationAxis);
+        //We use max function because it is used to limit the player speed.
+        float currentVelocity = Mathf.Max(
+            Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y),
+            Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x)
+        );
+        //Detecting changing of direction: the bool value is intended
+        //to perform a "brake" if the player wants to change direction
+        //after reaching a certain velocity.
+        bool isDirectionChanged = (isAccelerating && straightMovInput < 0)
+                || (!isAccelerating && straightMovInput > 0);
+        if (isDirectionChanged)
+        {
+            isAccelerating = !isAccelerating;
+            if(currentVelocity >= maxVelocity-0.5f)
+                GetComponent<Rigidbody2D>().AddForce(
+                    straightMovInput * brakeIntensity * transform.up, ForceMode2D.Impulse);
+        }
+        else if (straightMovInput != 0 && 
+            currentVelocity <= maxVelocity)
+            GetComponent<Rigidbody2D>().AddForce(
+                straightMovInput * enginePower * transform.up);
+
+        if (orientInput != 0 &&
+            Mathf.Abs(GetComponent<Rigidbody2D>().angularVelocity) <= maxAngularVelocity)
+        {
+            GetComponent<Rigidbody2D>().AddTorque(
+                orientInput * torqueIntensity);
+        }
+
+        //TODO: Shoot input
+    }
+}
