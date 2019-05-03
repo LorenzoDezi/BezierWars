7d10186cb5b86487fc43d73c4c54e78899aa33a9
Lorenzo Dezi <lorenzo.dezi@studenti.unicam.it>
2019-05-03 20:59:30 +0200

    enemySpawner sprite. Wall tilemap.

---
+++ Assets/Scripts/Background/BackgroundColorComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BackgroundColorComponent : MonoBehaviour
+{
+    [SerializeField]
+    private float colorShiftSpeed = -0.0005f;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 1f);
+    }
+
+    void Update()
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        var color = spriteRenderer.color;
+        color.b = Mathf.Clamp01(color.b + colorShiftSpeed);
+        color.r = Mathf.Clamp01(color.r - colorShiftSpeed);
+        if (color.b <= 0 || color.b >= 1) colorShiftSpeed = -colorShiftSpeed;
+        spriteRenderer.color = color;
+    }
+}
